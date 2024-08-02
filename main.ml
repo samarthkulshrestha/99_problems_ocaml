4ef5459754b0079c5241ee0d444b498e0fb9445e
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2024-08-03 03:58:28 +0530

    solution for problem 03

--- main.ml
+++ main.ml
@@ -12,3 +12,10 @@ let rec last_two (xs: 'a list): ('a * 'a) option =
   | [_] -> None
   | [x; y] -> Some (x, y)
   | _ :: rest -> last_two rest
+
+(** problem 03 **)
+let rec at (k: int) (xs: 'a list): 'a option =
+  match xs with
+  | x :: _ when k == 1 -> Some x
+  | _ :: rest when k > 1 -> at (k - 1) rest
+  | _ -> None
